710cf34e9e9b9de4d76041c2b941da0c62bfc2ec
utkarshsta44 <utkarsh.srivastava@successive.tech>
2023-12-22 20:54:59 +0530

    NZWalks third  commit

---
+++ NZWalks.Api/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Model.Domain;
+
+namespace NZWalks.Api.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        /*public NZWalksDbContext(DbContextOptions dbContextOptions):base()
+        { 
+        }*/
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        { }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        public  DbSet<Walk>Walks { get; set; }
+
+    }
+}
+
