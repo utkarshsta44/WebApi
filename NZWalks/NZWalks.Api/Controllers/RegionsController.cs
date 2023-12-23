c7e4000e9ca6aa4dc269a50ed20f94c69411f182
utkarshsta44 <utkarsh.srivastava@successive.tech>
2023-12-23 12:14:45 +0530

    Updated the RegionsController File

--- NZWalks/NZWalks.Api/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.Api/Controllers/RegionsController.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Data;
 using NZWalks.Api.Model.Domain;
+using System.Runtime.InteropServices;
 
 namespace NZWalks.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class RegionsController : ControllerBase
+
     {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext=dbContext;
+
+        }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions=new List<Region> {
-                new Region
+            /*var regions=new List<Region> {
+*//*                new Region
                 {
                     Id=Guid.NewGuid(),
                     Name="Auckland",
@@ -27,9 +36,10 @@ namespace NZWalks.Api.Controllers
                     Code="WelG",
                     RegionImageUrl="https://wellington.govt.nz/wellington-city/about-wellington-city/profile-of-wellington"
 
-                }
+                }*//*
             }; 
-
+*/
+            var regions = dbContext.Regions.ToList();
             return Ok(regions);
         }
     }
