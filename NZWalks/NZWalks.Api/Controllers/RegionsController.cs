d464815e80dacf060133f8b764be7071ff7fbefa
utkarshsta44 <utkarsh.srivastava@successive.tech>
2023-12-23 13:17:38 +0530

    Added Dto into the Domain folder of the Model Folder.

--- NZWalks/NZWalks.Api/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.Api/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.Api.Data;
 using NZWalks.Api.Model.Domain;
+using NZWalks.Api.Models.Domain.DTO;
 using System.Runtime.InteropServices;
 
 namespace NZWalks.Api.Controllers
@@ -17,30 +18,41 @@ namespace NZWalks.Api.Controllers
             this.dbContext=dbContext;
 
         }
-
         [HttpGet]
         public IActionResult GetAll()
-        {   //this is for the  data that is hardcoded above approach also use the dB data use.
-            /*var regions=new List<Region> {
-                new Region
-                {
-                    Id=Guid.NewGuid(),
-                    Name="Auckland",
-                    Code="Akl",
-                    RegionImageUrl="https://www.newzealand.com/in/auckland/"
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),    
-                    Name="Wellington",
-                    Code="WelG",
-                    RegionImageUrl="https://wellington.govt.nz/wellington-city/about-wellington-city/profile-of-wellington"
+        {
+            var regionsDomain = dbContext.Regions.ToList();
 
-                }
-            }; 
-*/
-            var regions = dbContext.Regions.ToList();
-            return Ok(regions);
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+
+           }
+            return Ok(regionsDto);
+        }
+        //get single element by passing the id
+        [HttpGet]
+        [Route("id:Guid")]
+        public IActionResult GetById([FromRoute]Guid id) {
+
+
+          //there is both the method for the getting the elemnt by id 1.one methd is the FIND method
+          //2.  the LINQ method  that use the FirstOrDefault.
+         //  var region= dbContext.Regions.Find(id);
+         var region = dbContext.Regions.FirstOrDefault(x=>x.Id==id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
         }
     }
 }
